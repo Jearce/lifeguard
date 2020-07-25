b586c073273bc98585ec849cf8938b2894174f6c
Jearce <jessiearce42@gmail.com>
2020-07-25 18:07:24 -0500

    Custom user model test passed and typos fixed

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -24,7 +24,7 @@ class UsersManagersTests(TestCase):
 
     def test_create_superuser(self):
         User = get_user_model()
-        admin_user = User.objects.create_superuser('superuser@user.com','foo')
+        admin_user = User.objects.create_superuser('super@user.com','foo')
         self.assertEqual(admin_user.email, 'super@user.com')
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
@@ -35,8 +35,8 @@ class UsersManagersTests(TestCase):
         except AttributeError:
             pass
         with self.assertRaises(ValueError):
-            User.objects.create_user(
-                email='superuser@user.com',
+            User.objects.create_superuser(
+                email='super@user.com',
                 password='foo',
                 is_superuser=False
             )
