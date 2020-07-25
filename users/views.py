eb15b9b9fed1070c7e62c25d2f324a3cd96370c8
Jearce <jessiearce42@gmail.com>
2020-07-25 17:39:43 -0500

    Created view to serve signup.html template

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class SignUpView(TemplateView):
+    template_name = 'users/signup.html'
