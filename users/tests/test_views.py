f53d2f95636567f48f6cda6ae95674715e9a8161
Jearce <jessiearce42@gmail.com>
2020-07-25 13:55:40 -0500

    First view test for user sign up template

---
+++ users/tests/test_views.py
@@ -0,0 +1,11 @@
+from django.urls import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+
+from .. import views
+
+class SignUpPageTest(TestCase):
+
+    def test_uses_signup_template(self):
+        response = self.client.get('/signup/')
+        self.assertTemplateUsed(response,'users/signup.html')
