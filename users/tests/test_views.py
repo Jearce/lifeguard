7e11f1ca302d757935f7891e7a83cea9702f7f69
Jearce <jessiearce42@gmail.com>
2020-07-25 19:40:09 -0500

    Changed test methods and added method to test view is routed to the correct url

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -3,9 +3,14 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from .. import views
+from ..models import User
 
-class SignUpPageTest(TestCase):
+class SignUpViewTest(TestCase):
 
-    def test_uses_signup_template(self):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/users/signup/')
+        self.assertEqual(response.status_code,200)
+
+    def test_view_uses_correct_template(self):
         response = self.client.get('/users/signup/')
         self.assertTemplateUsed(response,'users/signup.html')
