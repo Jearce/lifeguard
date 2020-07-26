157909df775f59aea1757e4176aa41c1282e2a66
Jearce <jessiearce42@gmail.com>
2020-07-25 19:08:45 -0500

    Change sign up view to CreateView

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import CreateView
+
+from .forms import CustomUserCreationForm
+from .models import User
 
 # Create your views here.
-class SignUpView(TemplateView):
+class SignUpView(CreateView):
+    model = User
+    form_class = CustomUserCreationForm
     template_name = 'users/signup.html'
