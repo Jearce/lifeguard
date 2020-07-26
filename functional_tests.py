abc24e383e8e5862a88a312eafbaf2cd3e010b79
Jearce <jessiearce42@gmail.com>
2020-07-25 19:07:44 -0500

    FT for user creating account

--- functional_tests.py
+++ functional_tests.py
@@ -12,8 +12,21 @@ class SignUpTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_sign_up(self):
-        self.browser.get('http://localhost:700/signup/')
+        self.browser.get('http://127.0.0.1:7000/users/signup/')
         self.assertIn('Sign-Up',self.browser.title)
+
+        test_email = 'test@example.com'
+        test_password = 'sdfne134d'
+
+        email = self.browser.find_element_by_id('id_email')
+        password1 = self.browser.find_element_by_id('id_password1')
+        password2 = self.browser.find_element_by_id('id_password2')
+
+        email.send_keys(test_email)
+        password1.send_keys(test_password)
+        password2.send_keys('not the same')
+        self.browser.find_element_by_id('signup-form').submit()
+        time.sleep(10)
 
 if __name__ == '__main__':
     unittest.main()
