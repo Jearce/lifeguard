28ebd70f62908b9df16b9560ff43a822e2d82d06
Jearce <jessiearce42@gmail.com>
2020-07-27 15:22:21 -0500

    Wrote test for redirect after user sign up

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,11 +1,15 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.contrib.auth import get_user_model
 
 from .. import views
 from ..models import User
 
 class SignUpViewTest(TestCase):
+
+    def setUp(self):
+        self.credentials = {'email':'test@example.com', 'password1':'2dhd7!42','password2':'2dhd7!42'}
 
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/users/signup/')
@@ -14,6 +18,16 @@ class SignUpViewTest(TestCase):
     def test_view_uses_correct_template(self):
         response = self.client.get('/users/signup/')
         self.assertTemplateUsed(response,'users/signup.html')
+
+    def test_signup(self):
+        response = self.client.post('/users/signup/',data=self.credentials)
+        users = get_user_model().objects.all()
+        self.assertEqual(users.count(),1)
+
+    def test_redirect_after_signup(self):
+        response = self.client.post('/users/signup/',data=self.credentials)
+        self.assertRedirects(response,'/users/dashboard/')
+
 
 class LogInViewTest(TestCase):
 
@@ -32,8 +46,3 @@ class LogInViewTest(TestCase):
     def test_login(self):
         user_login = self.client.login(**self.credentials)
         self.assertTrue(user_login)
-
-    def test_redirect_after_login(self):
-        response = self.client.post('/users/login/',**self.credentials)
-        self.assertRedirects(response,'/users/dashboard/')
-
