72b8b1302117c2b4523d6d55ed707219328e46d8
Jearce <jessiearce42@gmail.com>
2020-07-27 16:15:42 -0500

    Created Dashboard view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.urls import reverse,reverse_lazy
+from django.views.generic import CreateView,TemplateView
 from django.contrib.auth.views import LoginView
 
 from .forms import CustomUserCreationForm
@@ -10,6 +11,11 @@ class SignUpView(CreateView):
     model = User
     form_class = CustomUserCreationForm
     template_name = 'users/signup.html'
+    success_url = 'users/dashboard/'
 
 class LogInView(LoginView):
     template_name = 'users/login.html'
+    redirect_field_name = 'users/dashboard/'
+
+class DashboardView(TemplateView):
+    template_name = 'users/dashboard.html'
