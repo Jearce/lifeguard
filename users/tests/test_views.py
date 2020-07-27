6acc16e1a7523f5f5b876d68b299f6f480d3af24
Jearce <jessiearce42@gmail.com>
2020-07-26 21:15:12 -0500

    Added unit test for login view

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -14,3 +14,14 @@ class SignUpViewTest(TestCase):
     def test_view_uses_correct_template(self):
         response = self.client.get('/users/signup/')
         self.assertTemplateUsed(response,'users/signup.html')
+
+class LogInView(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/users/login/')
+        self.assertEqual(response.status_code,200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get('/users/login/')
+        self.assertTemplateUsed(response,'users/login.html')
+
