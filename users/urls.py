c83beb4ed379af910870a1303f2527bf38559f48
Jearce <jessiearce42@gmail.com>
2020-07-29 18:36:25 -0500

    url for password reset confirm is set up

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,16 @@ urlpatterns = [
     path('login/',views.LogInView.as_view(),name='login'),
     path('dashboard/',views.DashboardView.as_view(),name='dashboard'),
 
-    path('password_reset/',auth_views.PasswordResetView.as_view(template_name="users/password_reset_form.html"),name='password_reset')
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name="users/password_reset_form.html",
+             subject_template_name="users/password_reset_subject.txt",
+             email_template_name="users/password_reset_email.html"
+         ),
+         name='password_reset'),
+    path('password_reset_confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="users/password_reset_confirm.html"
+         ),
+         name='password_reset_confirm')
 ]
