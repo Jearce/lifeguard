5d14ff13bb712332e2e388acedccf5ec9c5984cb
Jearce <jessiearce42@gmail.com>
2020-07-30 17:25:15 -0500

    Included url to contact information form; for LG registration

--- lifeguard/urls.py
+++ lifeguard/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('home/',views.HomeView.as_view(),name='home'),
+    path('lifeguard-registration/contact-information',views.ContactInformationView.as_view(),name='contact_information')
 ]
