dfcd97db7f5874a063afa9a835a51de3458699ff
Jearce <jessiearce42@gmail.com>
2020-07-29 19:11:18 -0500

    Added urls for password reset done and complete

--- users/urls.py
+++ users/urls.py
@@ -20,5 +20,14 @@ urlpatterns = [
          auth_views.PasswordResetConfirmView.as_view(
              template_name="users/password_reset_confirm.html"
          ),
-         name='password_reset_confirm')
+         name='password_reset_confirm'),
+    path('password_reset_done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="users/password_reset_done.html"
+         ),
+         name='password_reset_done'),
+    path('password_reset_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="users/password_reset_complete.html"),
+         name='password_reset_complete')
 ]
