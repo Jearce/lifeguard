c63bf205cc853b607b4ce6e91cc464c440ae9490
Jearce <jessiearce42@gmail.com>
2020-07-30 18:16:29 -0500

    Added new contact information fields to user model

--- users/models.py
+++ users/models.py
@@ -7,6 +7,8 @@ from .managers import CustomUserManager
 class User(AbstractUser):
     username = None
     email = models.EmailField('email address',unique=True)
+    phone = models.CharField('phone number',max_length=16,blank=True,null=True)
+    dob = models.DateField('date of birth',max_length=8,blank=True,null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
