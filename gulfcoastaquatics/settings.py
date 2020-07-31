09eae8742d1c4d938d862669f389eb61db228c0e
Jearce <jessiearce42@gmail.com>
2020-07-31 08:12:47 -0500

    Created new secret key and moved to env variable

--- gulfcoastaquatics/settings.py
+++ gulfcoastaquatics/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bjn#s)qe7n49h($+5u6z&9&r+4&a6+!t&eia@f26i5y_((1*&6'
+SECRET_KEY = os.environ.get('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
