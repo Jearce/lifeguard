c8aed1995a077f6ceeb83c0ee2faad4536d1857b
Jearce <jessiearce42@gmail.com>
2020-07-30 22:43:09 -0500

    Added url to addresscreate form

--- lifeguard/urls.py
+++ lifeguard/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('home/',views.HomeView.as_view(),name='home'),
     path('lifeguard-registration/contact-information/<int:pk>/',views.ContactInformationUpdate.as_view(),name='contact_information'),
-    path('lifeguard-registration/emergency-contact/',views.EmergencyContactCreate.as_view(),name='emergency_contact')
+    path('lifeguard-registration/emergency-contact/',views.EmergencyContactCreate.as_view(),name='emergency_contact'),
+    path('lifeguard-registration/address/',views.AddressCreate.as_view(),name='address')
 ]
