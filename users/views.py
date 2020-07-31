5f4d88084433a79a1c20913a467a64ceddd0c519
Jessie Arce <jessiearce42@gmail.com>
2020-07-31 16:59:20 -0500

    Fixed sign up view so that user is logged in on successful sign in

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.urls import reverse,reverse_lazy
 from django.views.generic import CreateView,TemplateView
 from django.contrib.auth.views import LoginView
-from django.contrib.auth import logout
+from django.contrib.auth import logout,login,authenticate
 
 from .forms import CustomUserCreationForm
 from .models import User
@@ -12,7 +12,19 @@ class SignUpView(CreateView):
     model = User
     form_class = CustomUserCreationForm
     template_name = 'users/signup.html'
-    success_url = reverse_lazy('dashboard')
+    #success_url = reverse('dashboard')
+
+    def form_valid(self,form):
+        valid = super().form_valid(form)
+        print(form.cleaned_data)
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password1']
+        new_user = authenticate(email=email,password=password)
+        login(self.request,new_user)
+        return valid
+
+    def get_success_url(self):
+        return reverse_lazy('dashboard')
 
 class LogInView(LoginView):
     template_name = 'users/login.html'
