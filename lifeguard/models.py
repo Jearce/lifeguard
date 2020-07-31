e735367686992ab53d6c5337b114ec3b46ce2020
Jearce <jessiearce42@gmail.com>
2020-07-30 21:32:33 -0500

    Implemented emergency contact model

--- lifeguard/models.py
+++ lifeguard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class EmergencyContact(models.Model):
+    name = models.CharField('name',max_length=255)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    relationship = models.CharField('relationship',max_length=255)
+    phone = models.CharField('phone',max_length=255)
