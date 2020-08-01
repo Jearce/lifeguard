5b35bb395196ccc2f909ec2c6fca1b7118dc98fb
Jearce <jessiearce42@gmail.com>
2020-08-01 15:50:56 -0500

    Implemented lifeguard model

--- lifeguard/models.py
+++ lifeguard/models.py
@@ -15,3 +15,24 @@ class Address(models.Model):
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
     zip = models.CharField(max_length=255)
+
+class Lifeguard(models.Model):
+    WORK_CHOICES = [
+        ('Y',"Yes (I want the discounted employee rate)"),
+        ('N',"I will be working elsewhere (NOT eligible for discount)"),
+        ("O","Other")
+    ]
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    already_certified = models.BooleanField("Are you a certified lifeguard?",choices=[("Y","Yes"),("N","No")])
+    wants_to_work_for_company = models.CharField("Do you plan to work for Gulf Coast Aquatics?",choices=WORK_CHOICES,max_length=1)
+    payment_agreement = models.BooleanField("I agree")
+    payment_agreement_signature = models.CharField("Your name",max_length=255)
+    no_refunds_agreement = models.BooleanField("I agree")
+    electronic_signature = models.CharField("Electronic signature",max_length=255)
+    last_certified = models.DateField(blank=True,null=True)
+    certification = models.ImageField(blank=True,null=True)
+    online_portion_complete = models.BooleanField(default=True)
+
+
+
+
