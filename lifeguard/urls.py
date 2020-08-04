64b0a7dc1b64b369f51d49dfedf955ff7fa4c63c
Jearce <jessiearce42@gmail.com>
2020-08-04 13:31:24 -0500

    Changed emergency contact url to emergency contact create

--- lifeguard/urls.py
+++ lifeguard/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('home/',views.HomeView.as_view(),name='home'),
     path('lifeguard-registration/contact-information/<int:pk>/',views.ContactInformationUpdate.as_view(),name='contact_information'),
-    path('lifeguard-registration/emergency-contact/',views.EmergencyContactCreate.as_view(),name='emergency_contact'),
+    path('lifeguard-registration/emergency-contact-create/',views.EmergencyContactCreate.as_view(),name='emergency_contact_create'),
     path('lifeguard-registration/address/',views.AddressCreate.as_view(),name='address'),
     path('lifeguard-registration/lifeguard-information/',views.LifeguardCreate.as_view(),name='lifeguard_create'),
     path('lifeguard/classes/',views.LifeguardClasses.as_view(),name='classes'),
