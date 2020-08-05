29a2c53aaed0941a3610619832f7f07f8219fa9c
Jearce <jessiearce42@gmail.com>
2020-08-04 20:47:28 -0500

    Added emergency contact update url

--- users/urls.py
+++ users/urls.py
@@ -17,5 +17,6 @@ urlpatterns = [
 
     path('contact-information/<int:pk>/',views.ContactInformationUpdate.as_view(),name='contact_information'),
     path('emergency-contact-create/',views.EmergencyContactCreate.as_view(),name='emergency_contact_create'),
+    path('emergency-contact-update/',views.EmergencyContactUpdate.as_view(),name='emergency_contact_update'),
     path('address/',views.AddressCreate.as_view(),name='address'),
 ]
