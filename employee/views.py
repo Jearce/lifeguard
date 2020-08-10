913a97f7f23b6784183459e9f0c4a3cd202d1134
Jearce <jessiearce42@gmail.com>
2020-08-10 13:31:30 -0500

    Wrote EmployeeCreateOrUpdate view

--- employee/views.py
+++ employee/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from django.views.generic.edit import UpdateView
 from django.views.generic import TemplateView
 
+from .models import Employee
+
+from .forms import EmployeeForm
+
 # Create your views here.
-class EmployeeUpdate(TemplateView):
+class EmployeeCreateOrUpdate(UpdateView):
+    form_class = EmployeeForm
     template_name = 'employee/employee_form.html'
+
+    def get_object(self):
+        user = self.request.user
+        try:
+            obj = Employee.objects.get(pk=user)
+        except Employee.DoesNotExist:
+            obj = Employee(user)
+        return obj
