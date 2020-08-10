7ae94186cea5f49631d8f91138b21fdf3bfa5ccf
Jearce <jessiearce42@gmail.com>
2020-08-10 18:23:17 -0500

    Modified employee form have radio select widgets and dateinput

--- employee/forms.py
+++ employee/forms.py
@@ -1,8 +1,18 @@
-from django.forms import ModelForm
+from django.forms import ModelForm,DateInput,RadioSelect,CheckboxSelectMultiple
 from .models import Employee
 
+
+BOOLEAN_CHOICES = [(True,"Yes"),(False,"No")]
 class EmployeeForm(ModelForm):
+
     class Meta:
         model = Employee
         exclude = ('user',)
-
+        widgets = {
+            'applied_position':RadioSelect,
+            'start_date':DateInput(attrs={'type':'date'}),
+            'end_date':DateInput(attrs={'type':'date'}),
+            "work_authorization":RadioSelect,
+            "charged_or_arrested":RadioSelect,
+            "has_felony":RadioSelect,
+        }
