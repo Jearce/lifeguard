364acd37fa0581d346a140aa7b11babb441aa25d
Jearce <jessiearce42@gmail.com>
2020-08-10 18:24:40 -0500

    Completed employee model to working form

--- employee/models.py
+++ employee/models.py
@@ -6,21 +6,54 @@ class Transportation(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 class Employee(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
-    home_phone = models.CharField(max_length=12)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    home_phone = models.CharField(max_length=12,blank=True,null=True)
     who_referred_you = models.TextField("Who referred you to us?")
-    transpotation = models.ForeignKey(Transportation,on_delete=models.CASCADE)
-    CHOICES = [('L','Lifeguard'),('S','Supervisor'),('M','Manager')]
-    applied_position = models.CharField(choices=CHOICES,max_length=1)
-    work_hours_desired = models.PositiveIntegerField()
-    desired_pay_rate = models.DecimalField(max_digits=6, decimal_places=2)
-    work_authorization = models.CharField(max_length=255)
-    charged_or_arrested = models.BooleanField()
-    has_felony = models.BooleanField()
-    contract_employment_agreement = models.CharField(max_length=255)
+    transportation = models.ForeignKey(
+        "Transportation",
+        on_delete=models.CASCADE
+    )
+
+
+    POSITION_CHOICES = [('L','Lifeguard'),('S','Supervisor'),('M','Manager')]
+    applied_position = models.CharField(
+        "Position applying for",
+        choices=POSITION_CHOICES,
+        max_length=1,
+        blank=False,
+        default=None,
+    )
+
+    start_date = models.DateField("Date available to start")
+    end_date = models.DateField(
+        "Ending date - This is not official. A notice is still required."
+    )
+
+    work_hours_desired = models.PositiveIntegerField(
+        "Number of hours desired per week"
+    )
+    desired_pay_rate = models.DecimalField(
+        "Asking rate of pay",
+        max_digits=6,
+        decimal_places=2
+    )
+    pool_preference = models.CharField("Pool preference",max_length=255,default="")
+    subdivision = models.CharField(
+        "Which subdivision do you live in or nearby",
+        max_length=255
+    )
+
+    BOOLEAN_CHOICES = [(True,"Yes"),(False,"No")]
+    work_authorization = models.BooleanField('',choices=BOOLEAN_CHOICES,blank=False,default=None)
+    charged_or_arrested = models.BooleanField('',choices=BOOLEAN_CHOICES,blank=False,default=None)
+    has_felony = models.BooleanField('',choices=BOOLEAN_CHOICES,blank=False,default=None)
+    contract_employment_agreement = models.BooleanField("I agree")
     electronic_signature = models.CharField("Your name",max_length=255)
-    subdivision = models.CharField(max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.email
