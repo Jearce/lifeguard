651c21b075ed774d707ad27e0bed8e82c251b8aa
Jearce <jessiearce42@gmail.com>
2020-08-12 17:16:44 -0500

    Added job history url

--- employee/urls.py
+++ employee/urls.py
@@ -5,4 +5,5 @@ app_name = 'employee'
 urlpatterns = [
     path('create/',views.EmployeeCreateOrUpdate.as_view(),name="create"),
     path('education/',views.EmployeeEducation.as_view(),name="education"),
+    path('job-history/',views.JobHistory.as_view(),name="job_history")
 ]
