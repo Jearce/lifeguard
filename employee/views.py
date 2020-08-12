009be4e7c1cf43476e5c5329ca72015a80b32ff1
Jearce <jessiearce42@gmail.com>
2020-08-12 14:24:32 -0500

    Created Employee education view with inline formset

--- employee/views.py
+++ employee/views.py
@@ -3,9 +3,9 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 from django.views.generic import TemplateView
 
-from .models import Employee
+from .models import Employee,EmployeeEducation
 
-from .forms import EmployeeForm
+from .forms import EmployeeForm,EducationInlineFormset,EducationForm
 
 # Create your views here.
 class EmployeeCreateOrUpdate(UpdateView):
@@ -23,5 +23,34 @@ class EmployeeCreateOrUpdate(UpdateView):
     def get_success_url(self):
         return reverse_lazy('employee:education')
 
-class EmployeeEducation(TemplateView):
+class EmployeeEducation(UpdateView):
+    model = EmployeeEducation
     template_name = 'employee/education_form.html'
+    form_class = EducationForm
+
+    def get_object(self):
+        employee  = Employee.objects.get(user=self.request.user)
+        return employee
+
+    def get_context_data(self,**kwargs):
+        context = {}
+        formset = EducationInlineFormset(
+            instance=self.get_object()
+        )
+        context['formset'] = formset
+        return context
+
+    def post(self, request,*args,**kwargs):
+        formset = EducationInlineFormset(
+            request.POST,
+            instance=self.get_object()
+        )
+        if formset.is_valid():
+            formset.save()
+            return self.form_valid(formset)
+        return render(request,self.template_name,context={'formset':formset})
+
+    def get_success_url(self):
+        return reverse_lazy('employee:education')
+
+
