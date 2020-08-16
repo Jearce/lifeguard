d652698bfe252d1135997df254c63702f3d7efa4
Jearce <jessiearce42@gmail.com>
2020-08-15 20:39:44 -0500

    Created url for lifeguard registration view

--- lifeguard/urls.py
+++ lifeguard/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create/',views.LifeguardCreateOrUpdate.as_view(),name='create'),
     path('already-certified/',views.LifeguardCertified.as_view(),name='already_certified'),
     path('classes/',views.LifeguardClasses.as_view(),name='classes'),
-    path('classes/<int:pk>/',views.LifeguardClasses.as_view(),name='classes')
+    path('classes/<int:pk>/',views.LifeguardClasses.as_view(),name='classes'),
+    path('registration/',views.lifeguard_registration,name='registration'),
 ]
