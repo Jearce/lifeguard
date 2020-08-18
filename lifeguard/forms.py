f6f05d148fc4b3a9ea52d11893c1bb0c398de2cf
Jearce <jessiearce42@gmail.com>
2020-08-18 11:55:49 -0500

    Create lifeguard form

--- lifeguard/forms.py
+++ lifeguard/forms.py
@@ -9,3 +9,14 @@ class LifeguardCertifiedForm(ModelForm):
             'last_certified':DateInput(attrs={'type':'date'})
         }
 
+class LifeguardForm(ModelForm):
+    class Meta:
+        model = Lifeguard
+        fields = [
+            'already_certified',
+            'wants_to_work_for_company',
+            'payment_agreement',
+            'payment_agreement_signature',
+            'no_refunds_agreement',
+            'electronic_signature'
+        ]
