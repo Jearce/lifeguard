25da92acaf05e2263959014b622b19958aac94f3
Jearce <jessiearce42@gmail.com>
2020-08-21 13:36:00 -0500

    Used crispy forms FormHelper to make fancy form for contact information

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,9 @@ from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.forms import ModelForm,modelformset_factory,inlineformset_factory,DateInput
 from .models import EmergencyContact,User
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout,Div,Submit,Field,Button
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = User
@@ -20,10 +23,30 @@ class EmergencyContactForm(ModelForm):
 class ContactInformationForm(ModelForm):
     class Meta:
         model = User
-        fields = ['email', 'first_name', 'last_name','phone','dob']
+        fields = ['first_name', 'last_name','email','phone','dob']
         widgets = {
             "dob":DateInput(attrs={'type':'date'})
         }
+
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.form_class = "center-form"
+        self.helper.form_id = "contact_information_form"
+        self.helper.layout = Layout(
+            Div("email",css_class="form-group"),
+            Div(
+                Div("first_name",css_class="form-group col"),
+                Div("last_name",css_class="form-group col"),
+                css_class="form-row"
+            ),
+            Div("phone",css_class="form-group"),
+            Div("dob",css_class="form-group"),
+            Submit('','Submit')
+        )
+
 
 EmergencyContactFormSet = modelformset_factory(
     EmergencyContact,
