57a43a63e516744dc577a1d2756c533eacad4655
Jearce <jessiearce42@gmail.com>
2020-08-23 15:55:47 -0500

    Test for employee model and that in user model the is_employee field is set to true when user fills out employee form

---
+++ employee/tests/test_models.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from users.models import User
+from employee.models import Employee,Position,Transportation
+
+
+class EmployeeTest(TestCase):
+    def setUp(self):
+        self.position1 = Position.objects.create(title="Lifeguard",minimum_age=15,lifeguard_required=True)
+        self.position2 = Position.objects.create(title="Supervisor",minimum_age=18,lifeguard_required=False)
+        self.transportation = Transportation.objects.create(name="Car",description="I will drive by car")
+        self.user = User.objects.create_user(email="test@example.com",password="t3es3t123!")
+        self.employee = Employee.objects.create(
+            user=self.user,
+            who_referred_you="A friend",
+            transportation=self.transportation,
+            start_date=datetime.now(),
+            end_date=datetime.now(),
+            work_hours_desired=40,
+            desired_pay_rate=18.5,
+            pool_preference="Village Pool",
+            subdivision="Village Subdivision",
+            work_authorization=True,
+            charged_or_arrested=False,
+            has_felony=False,
+            contract_employment_agreement=True,
+            electronic_signature="Larry Berry",
+            is_hired=True
+        )
+
+    def test_user_is_employee(self):
+        self.employee.save()
+        self.assertTrue(self.user.is_employee)
+
