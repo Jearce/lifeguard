a9d5008ee237f4e49f5cf1112117fd74d6f0a154
Jearce <jessiearce42@gmail.com>
2020-08-23 16:37:56 -0500

    Replaced CommonSetUp for TestCase in JobHistoryTest

--- employee/tests/test_views.py
+++ employee/tests/test_views.py
@@ -181,8 +181,12 @@ class EmployeeEducationTest(CommonSetUp):
         self.assertEqual(EmployeeEducation.objects.count(),2)
         self.assertRedirects(response,reverse('employee:job_history'))
 
-class JobHistoryTest(CommonSetUp):
+class JobHistoryTest(TestCase):
     def setUp(self):
+        self.email = 'test@example.com'
+        self.password = 'asdhf33!'
+        self.user = User.objects.create_user(email=self.email, password=self.password)
+
         self.client.login(email=self.email,password=self.password)
         self.create_job_history_data = [{
             "previous_employer":"Some Company",
@@ -214,7 +218,37 @@ class JobHistoryTest(CommonSetUp):
                 "employee":'1',
                 "id":''
             }
+
         ]
+        self.new_lifeguard = {
+            "already_certified":False,#new lifeguard
+            "wants_to_work_for_company":True,#should be redirected to employee register page
+            "payment_agreement":True,
+            "payment_agreement_signature":"Larry Johnson",
+            "no_refunds_agreement":True,
+            "electronic_signature":"Larry Johnson",
+        }
+        self.employee_data = {
+            "home_phone":"712 634 3328",
+            "who_referred_you":"A friend.",
+            "transportation":"1",
+            "start_date":"2020-08-09",
+            "end_date":"2020-12-10",
+            "work_hours_desired":"40",
+            "desired_pay_rate":"17.50",
+            "pool_preference":"Village Pool",
+            "subdivision":"My subdivision 122",
+            "work_authorization":True,
+            "charged_or_arrested":False,
+            "has_felony":False,
+            "contract_employment_agreement":True,
+            "electronic_signature":"Larry Johnson",
+        }
+        self.position1 = Position.objects.create(title="Lifeguard",minimum_age=15,lifeguard_required=True)
+        self.position2 = Position.objects.create(title="Supervisor",minimum_age=18,lifeguard_required=False)
+        self.transportation = Transportation.objects.create(name="Car",description="I will drive by car")
+
+
 
     def test_view_url_exists_at_desired_location(self):
         employee = Employee(
@@ -346,7 +380,6 @@ class JobHistoryTest(CommonSetUp):
         response = self.client.post(reverse('employee:job_history'),form_data)
         self.assertEqual(response.status_code,302)
         self.assertRedirects(response,reverse('lifeguard:create'))
-
 
     def test_redirect_for_already_lifeguard(self):
         lifeguard = Lifeguard.objects.create(user=self.user,**self.new_lifeguard)
