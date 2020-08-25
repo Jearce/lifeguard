92884ea7d8167b93b62eb2acca0a95d1c38da491
Jearce <jessiearce42@gmail.com>
2020-08-25 14:49:04 -0500

    url for application status view

--- employee/urls.py
+++ employee/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('create/',views.EmployeeCreateOrUpdate.as_view(),name="create"),
     path('education/',views.EmployeeEducation.as_view(),name="education"),
     path('job-history/',views.JobHistory.as_view(),name="job_history"),
-    path('registration/',views.employee_registration,name="registration")
+    path('registration/',views.employee_registration,name="registration"),
+    path('application-status/',views.ApplicationStatus.as_view(),name="application_status"),
 ]
