bebf7be3530b4a9746b716f912a926ba276f7bb1
Jearce <jessiearce42@gmail.com>
2020-08-31 19:33:55 -0500

    Test for employee checklist form

--- employee/tests/test_forms.py
+++ employee/tests/test_forms.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from employee.forms import EducationForm
+from employee import forms
+
+from employee.tests.helpers import CommonSetUp
 
 class EmployeeEducationFormTest(TestCase):
     def test_forget_to_finish_filling_out_date_leaving_to_college(self):
@@ -9,7 +11,7 @@ class EmployeeEducationFormTest(TestCase):
             'attending_college':True,
             'date_leaving_to_college':'',
         }
-        form = EducationForm(data=data)
+        form = forms.EducationForm(data=data)
         errors = form.errors
         self.assertEqual(errors["date_leaving_to_college"],["Please fill out the date you will be leaving."])
 
@@ -20,10 +22,22 @@ class EmployeeEducationFormTest(TestCase):
             'attending_college':'',
             'date_leaving_to_college':'2020-08-21',
         }
-        form = EducationForm(data=data)
+        form = forms.EducationForm(data=data)
         errors = form.errors
         self.assertEqual(errors["attending_college"],["This field is required."])
 
+class EmployeeCheckListFormTest(CommonSetUp):
+    def setUp(self):
+        super().setUp()
+        #already submitted application
+        self.employee = self.create_employee()
+        self.employee.is_hired = True
+        self.employee.save()
 
+    def test_email_address_already_filled_in(self):
+        form = forms.ChecklistForm(user=self.user)
+        self.assertEqual(self.user.email,form.fields['email_address'].initial)
+
+    def test_cannot_fill_wavier_and_submit_vaccination_record(self):
 
 
