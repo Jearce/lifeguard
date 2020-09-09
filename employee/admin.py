a79cf1f7c1bac7f7aacb9374895ebf2e0af42204
Jearce <jessiearce42@gmail.com>
2020-09-09 14:47:30 -0500

    Started customization for employee admin interface

--- employee/admin.py
+++ employee/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from employee import models
 
 # Register your models here.
-admin.site.register(models.Employee)
+@admin.register(models.Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    def formfield_for_manytomany(self,db_field,request,**kwargs):
+        if db_field.name == 'applied_positions':
+            kwargs["queryset"] = models.Position.objects.filter(employee=request.user.employee)
+        return super().formfield_for_manytomany(db_field,request,**kwargs)
+
 admin.site.register(models.Transportation)
 admin.site.register(models.Position)
 admin.site.register(models.Checklist)
