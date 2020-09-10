7f83b414347e690376b784c02fed2f089d4a0f90
Jearce <jessiearce42@gmail.com>
2020-09-10 18:20:08 -0500

    Included custom list display field to identify what class a lifeguard may need to take

--- lifeguard/admin.py
+++ lifeguard/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from . import models
+from lifeguard import models
+
+from dateutil.relativedelta import relativedelta
 
 # Register your models here.
-admin.site.register(models.Lifeguard)
+@admin.register(models.Lifeguard)
+class LifeguardAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'class_needed',
+    )
+
+    def class_needed(self,obj):
+        exper = obj.get_experience().years
+        if obj.needs_review():
+            return "needs review"
+        elif obj.get_experience().years >= 1 and obj.user.is_employee:
+            return "needs refresher"
+        elif exper == 0 and not obj.already_certified:
+            return "certificate class"
+
 admin.site.register(models.LifeguardClass)
 admin.site.register(models.Enroll)
