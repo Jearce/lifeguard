048e2b61019cca1aeb22fc3a3b7dba51d9973337
Jearce <jessiearce42@gmail.com>
2020-09-11 15:58:00 -0500

    created view for new checkout

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from payment.gateway import generate_client_token
+
 # Create your views here.
+def new_checkout(request):
+    client_token = generate_client_token()
+    return render(request,'payment/new_checkout.html',context={'client_token':client_token})
+
