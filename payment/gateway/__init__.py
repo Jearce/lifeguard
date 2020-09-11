4d81125ce03a77b4ab3d11e0cf14ccf8c75c3000
Jearce <jessiearce42@gmail.com>
2020-09-11 15:57:35 -0500

    Set up gateway to successfully display drop-in fields

--- payment/gateway/__init__.py
+++ payment/gateway/__init__.py
@@ -1,14 +1,16 @@
 import json
+import os
 
-from djagon.conf import settings
+from django.conf import settings
 
 import braintree
 
-if DEBUG:
-    secrets_file = '../config.json'
+
+if settings.DEBUG:
+    secrets_file = 'config.json'
 
 with open(secrets_file) as f:
-    SECRETS = json.loads(f)
+    SECRETS = json.loads(f.read())
 
 gateway = braintree.BraintreeGateway(
     braintree.Configuration(
@@ -18,3 +20,6 @@ gateway = braintree.BraintreeGateway(
         private_key=SECRETS["BT_PRIVATE_KEY"],
     )
 )
+
+def generate_client_token():
+    return gateway.client_token.generate()
