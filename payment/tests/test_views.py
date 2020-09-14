eee674e5012c5d0e8891ad122e070409056b657c
Jearce <jessiearce42@gmail.com>
2020-09-14 17:26:16 -0500

    First payment app test. Testing enrollment cart view

---
+++ payment/tests/test_views.py
@@ -0,0 +1,17 @@
+from django.urls import reverse
+from django.test import TestCase
+
+from payment import views
+
+from utils.test.helpers import create_user
+
+class EnrollmentCartTest(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get(reverse('payment:enrollment_cart'))
+        self.assertEqual(response.status_code,200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('payment:enrollment_cart'))
+        self.assertTemplateUsed(response,'payment/enrollment_cart.html')
+
