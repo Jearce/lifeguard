27df1b6c5de899329a4868df35494dd47080603c
Jearce <jessiearce42@gmail.com>
2020-09-13 22:16:49 -0500

    Finished experiment with braintree set up. Will remove this experiment and reimplement with tests

--- payment/views.py
+++ payment/views.py
@@ -1,9 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-from payment.gateway import generate_client_token
+import braintree
+
+from payment.forms import PaymentForm
+from payment.gateway import generate_client_token, transact, find_transaction
+
+
+TRANSACTION_SUCCESS_STATUSES = [
+    braintree.Transaction.Status.Authorized,
+    braintree.Transaction.Status.Authorizing,
+    braintree.Transaction.Status.Settled,
+    braintree.Transaction.Status.SettlementConfirmed,
+    braintree.Transaction.Status.SettlementPending,
+    braintree.Transaction.Status.Settling,
+    braintree.Transaction.Status.SubmittedForSettlement
+]
 
 # Create your views here.
 def new_checkout(request):
     client_token = generate_client_token()
-    return render(request,'payment/new_checkout.html',context={'client_token':client_token})
+    form = PaymentForm()
+    return render(request,'payment/new_checkout.html',context={'client_token':client_token,'form':form})
+
+def create_checkout(request):
+    if request.method == "POST":
+        form = PaymentForm(request.POST)
+        if form.is_valid():
+
+            lifeguard = request.user.lifeguard
+            cost = sum(enroll.lifeguard_class.cost  for enroll in lifeguard.enroll_set.all())
+
+            result = transact({
+                "amount": cost,
+                'payment_method_nonce':form.cleaned_data["nonce"],
+                'options':{
+                    'submit_for_settlement':True,
+                }
+            })
+
+            return redirect("payment:show_checkout",transaction_id=result.transaction.id)
+
+def show_checkout(request,transaction_id):
+    transaction = find_transaction(transaction_id)
+    result = {"header":"Success","transaction":transaction}
+    return render(request,"payment/show.html",context=result)
+
+
 
