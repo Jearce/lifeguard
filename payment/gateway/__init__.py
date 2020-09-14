af08e7949fead2dcce037fd813c1d2b17d4759db
Jearce <jessiearce42@gmail.com>
2020-09-13 22:14:34 -0500

    Fixed typo

--- payment/gateway/__init__.py
+++ payment/gateway/__init__.py
@@ -25,7 +25,7 @@ def generate_client_token():
     return gateway.client_token.generate()
 
 def transact(options):
-    return gateway.transact.sale(options)
+    return gateway.transaction.sale(options)
 
 def find_transaction(id):
-    return gateway.transact.find(id)
+    return gateway.transaction.find(id)
