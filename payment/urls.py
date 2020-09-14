fb645286cecf79ccb369e5176cbb2e68df7157fb
Jearce <jessiearce42@gmail.com>
2020-09-14 17:03:54 -0500

    Added url for user to see the classes they have enrolled and allow them to drop enrollments before paying for a class

--- payment/urls.py
+++ payment/urls.py
@@ -4,7 +4,9 @@ from payment import views
 
 app_name = "payment"
 urlpatterns = [
-    path('new/',views.new_checkout,name="new_checkout"),
-    path('checkout/<transaction_id>',views.show_checkout,name="show_checkout"),
-    path("checkout/",views.create_checkout,name="create_checkout"),
+    path("lifeguard-class/",views.EnrollmentCart.as_view(),name="lifeguard_class"),
+
+    #path('new/',views.new_checkout,name="new_checkout"),
+    #path('checkout/<transaction_id>',views.show_checkout,name="show_checkout"),
+    #path("checkout/",views.create_checkout,name="create_checkout"),
 ]
