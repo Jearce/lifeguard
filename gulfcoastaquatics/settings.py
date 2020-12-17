70c49f607e2707487d9caa600c5616992f927b89
Jearce <jessiearce42@gmail.com>
2020-12-17 13:39:31 -0600

    switched db to postgresql

--- gulfcoastaquatics/settings.py
+++ gulfcoastaquatics/settings.py
@@ -90,11 +90,14 @@ WSGI_APPLICATION = 'gulfcoastaquatics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DBNAME'),
+        'USER': os.getenv('DBUSER'),
+        'PASSWORD': os.getenv('DBPASS'),
+        'HOST':os.getenv('DBHOST'),
+        'PORT':os.getenv('DBPORT')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
