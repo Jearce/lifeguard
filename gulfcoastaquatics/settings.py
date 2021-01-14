9c0a74f9ab4632a7efdd2160182408fe59ead18b
Jearce <jessiearce42@gmail.com>
2021-01-13 18:55:23 -0600

    removed staticfiles_dir variable

--- gulfcoastaquatics/settings.py
+++ gulfcoastaquatics/settings.py
@@ -168,7 +168,7 @@ else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 if not DEBUG:
     import dj_database_url
