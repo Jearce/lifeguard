0f8c6a8b15d4e4c1bf9c6d717c9cfe3c1815fe3a
Jearce <jessiearce42@gmail.com>
2021-01-13 20:51:03 -0600

    added custom admin list and filter options for lifeguard class lessions

--- lifeguard/admin.py
+++ lifeguard/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django import forms
 from lifeguard import models
 
 from dateutil.relativedelta import relativedelta
 
 # Register your models here.
+
+
 @admin.register(models.Lifeguard)
 class LifeguardAdmin(admin.ModelAdmin):
     list_display = (
@@ -12,7 +15,7 @@ class LifeguardAdmin(admin.ModelAdmin):
         'years_since_last_certified'
     )
 
-    def class_needed(self,obj):
+    def class_needed(self, obj):
         exper = obj.get_experience().years
         if obj.needs_review():
             return "needs review"
@@ -21,16 +24,36 @@ class LifeguardAdmin(admin.ModelAdmin):
         elif exper == 0 and not obj.already_certified:
             return "certificate class"
 
-    def delete_queryset(self,request,queryset):
+    def delete_queryset(self, request, queryset):
         for e in queryset:
             e.user.is_lifeguard = False
             e.user.save()
-        return super().delete_queryset(request,queryset)
+        return super().delete_queryset(request, queryset)
 
     def years_since_last_certified(self, obj):
         if obj.already_certified:
             return obj.get_experience().years
 
+
+class LifeguardClassSessionForm(forms.ModelForm):
+
+    class Meta:
+        fields = '__all__'
+        model = models.LifeguardClassSession
+
+    def clean(self):
+        start_date = self.cleaned_data.get('start_time')
+        end_date = self.cleaned_data.get('end_time')
+        if start_date > end_date:
+            raise forms.ValidationError("Start time must be before end time")
+        return self.cleaned_data
+
+
+@admin.register(models.LifeguardClassSession)
+class LifeguardClassSessionAdmin(admin.ModelAdmin):
+    form = LifeguardClassSessionForm
+    list_display = ('lifeguard_class','date', 'start_time', 'end_time',)
+    list_filter = ('lifeguard_class',)
 
 
 admin.site.register(models.LifeguardClass)
