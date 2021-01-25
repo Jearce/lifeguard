0c37185a738f66da0b717d93a49eea9c464f7504
Jearce <jessiearce42@gmail.com>
2021-01-24 21:57:36 -0600

    added search for first and last name in lifeguard admin view. Added class needed filters

--- lifeguard/admin.py
+++ lifeguard/admin.py
@@ -5,11 +5,33 @@ from django.utils.html import format_html
 
 from lifeguard import models
 
+class LifeguardClassFilter(admin.SimpleListFilter):
+    title = ('lifeguard class')
+    parameter_name = "class needed"
+
+    def lookups(self,request,model_admin):
+        return (
+            ('review',"needs review"),
+            ('refresher',"needs refresher"),
+            ('lifeguard',"needs lifeguard class"),
+        )
+
+    def queryset(self,request,queryset):
+        if self.value() == "review":
+            return queryset.filter(id__in=[ obj.id for obj in queryset if obj.needs_review()])
+        if self.value() == "refresher":
+            return queryset.filter(id__in=[obj.id for obj in queryset if (obj.get_experience().days > 1 or obj.get_experience().years >= 1) and obj.user.is_employee])
+        if self.value() == "lifeguard":
+            return queryset.filter(id__in=[obj.id for obj in queryset if obj.certificate_expired() or not obj.already_certified])
+
+
 
 @admin.register(models.Lifeguard)
 class LifeguardAdmin(admin.ModelAdmin):
     list_display = (
         'user',
+        'first_name',
+        'last_name',
         'class_needed',
         'already_certified',
         'certificate_expires_on',
@@ -17,14 +39,19 @@ class LifeguardAdmin(admin.ModelAdmin):
         'employee_application'
     )
 
+    search_fields = ('user__email','user__phone','user__first_name','user__last_name')
+
+    list_filter = (LifeguardClassFilter,)
+
+
     def class_needed(self, obj):
-        exper = obj.get_experience().years
         if obj.needs_review():
             return "needs review"
-        elif (obj.get_experience().days > 1 or obj.get_experience.years >= 1) and obj.user.is_employee:
-            return "needs refresher"
-        elif exper == 0 and not obj.already_certified:
+        elif obj.certificate_expired() or not obj.already_certified:
             return "certificate class"
+        elif obj.user.is_employee:
+            return "needs refresher"
+
 
     def delete_queryset(self, request, queryset):
         for e in queryset:
@@ -32,12 +59,23 @@ class LifeguardAdmin(admin.ModelAdmin):
             e.user.save()
         return super().delete_queryset(request, queryset)
 
+
     def certificate_expires_on(self, obj):
         if obj.already_certified:
             return obj.date_certificate_expires
 
+
+    def first_name(self,obj):
+        return obj.user.first_name
+
+
+    def last_name(self,obj):
+        return obj.user.last_name
+
+
     def wants_to_work(self, obj):
         return "Yes" if obj.wants_to_work_for_company else "No"
+
 
     def employee_application(self,obj):
         if obj.wants_to_work_for_company and obj.user.is_employee:
