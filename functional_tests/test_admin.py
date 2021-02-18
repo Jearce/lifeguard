614e454dd539cb155550e948f15dc2e2402d6ed3
Jearce <jessiearce42@gmail.com>
2021-02-18 17:34:19 -0600

    functional test for custom admin panel

---
+++ functional_tests/test_admin.py
@@ -0,0 +1,33 @@
+import time
+
+from django.conf import settings
+
+from employee.models import Employee, Transportation, Position
+
+from functional_tests.helpers import BaseTestLoginFixture
+from functional_tests import helpers
+from utils.test.helpers import create_user
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+BASE_DIR = settings.BASE_DIR
+
+
+class AdminTest(BaseTestLoginFixture):
+    def setUp(self):
+        super().setUp()
+        self.user.is_superuser = True
+        self.user.save()
+
+
+    def test_is_super_user(self):
+        self.assertTrue(self.user.is_superuser)
+
+
+    def test_has_access_to_admin_panel(self):
+        self.login()
+        self.browser.find_element_by_id("id_admin_panel").click()
+        self.assertIn("admin-panel",self.browser.current_url)
