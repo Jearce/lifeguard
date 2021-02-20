0ce026d654ccef3d05f160960617bedb7e5133b2
Jearce <jessiearce42@gmail.com>
2021-02-19 23:33:22 -0600

    added 404 custom view

--- gulfcoastaquatics/urls.py
+++ gulfcoastaquatics/urls.py
@@ -18,6 +18,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path,include
 from django.views.generic import TemplateView
+from django.conf.urls import handler404
+from django.shortcuts import render
+
+
+def error_404(request,exception):
+    data = {"user": request.user}
+    return render(request,'404.html',data)
+
+handler404 = error_404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +35,7 @@ urlpatterns = [
     path('employee/',include('employee.urls')),
     path('payment/',include('payment.urls')),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
